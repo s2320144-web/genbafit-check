cc03358eed8b8e75217fa8ad8a5cbd468a9a1802
s2320144-web <s2320144@stu.musashino-u.ac.jp>
2026-03-20 01:18:32 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const LIFF_ID = "2009527083-MXJru0ta";
-
-const state = {
+const GAS_URL = "https://script.google.com/macros/s/AKfycbwkYyPLyzw9p_g6fVOCvuK74SzVJ3evPHOmZ1sxDtCyM2X9c85fUz3MkIxpclcpid9DTw/exec";const state = {
   step: 1,
   noPain: false,
   part: "",
