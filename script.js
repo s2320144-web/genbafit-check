f4163da3fbb95163144329507d6a1e641f6f708c
s2320144-web <s2320144@stu.musashino-u.ac.jp>
2026-03-27 01:37:58 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const LIFF_ID = "2009527083-MXJru0ta";
-const GAS_URL = "https://script.google.com/macros/s/AKfycbx7A3kj4H5yE3oGC9B-X212O_SUVhH_HiheGGcAtH5MITL94-8-ATN-HmhdmtAjmhPA/exec";const state = {
-  step: 1,
+const GAS_URL = "https://script.google.com/macros/s/AKfycbx7A3kj4H5yE3oGC9B-X212O_SUVhH_HiheGGcAtH5MITL94-8-ATN-HmhdmtAjmhPA/exec";  step: 1,
   noPain: false,
   part: "",
   otherPart: "",
